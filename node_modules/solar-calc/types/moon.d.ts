45e1cc58961ffae5e7ce24337f8a0497e4200428
rfdazzle <70452098+rfdazzle@users.noreply.github.com>
2023-03-22 13:31:55 -0400

    fix prereq

--- node_modules/solar-calc/types/moon.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Moon {
-  julianDate: number;
-  date: Date;
-  latitude: number;
-  longitude: number;
-  readonly illuminosity: number;
-  readonly distance: number;
-  constructor(date: Date, latitude: number, longitude: number);
-}
--- node_modules/solar-calc/types/sun.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Sun {
-  julianDate: number;
-  date: Date;
-  latitude: number;
-  longitude: number;
-  readonly solarNoon: Date;
-  constructor(date: Date, latitude: number, longitude: number);
-  timeAtAngle(angle, rising): Date | "error";
-}
